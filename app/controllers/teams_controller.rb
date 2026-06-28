91b538fc16a461a84682de905a620f656ff2a2ea
Daniel Lirio <daniellirioreis@gmail.com>
2026-06-28 02:47:08 +0000

    fix: zero spacing between credentials on PDF - badges flush edge to edge

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -50,7 +50,7 @@ class TeamsController < ApplicationController
                formats: [:html],
                page_size: "A4",
                orientation: "Portrait",
-               margin: { top: 2, bottom: 2, left: 2, right: 2 },
+               margin: { top: 0, bottom: 0, left: 0, right: 0 },
                disposition: "attachment"
       end
     end
