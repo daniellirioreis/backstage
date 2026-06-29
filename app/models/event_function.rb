152175086509502d920420b8af075438982e7e9e
Daniel Lirio <daniellirioreis@gmail.com>
2026-06-29 02:33:42 +0000

    feat: add to_s to EventFunction returning name

--- app/models/event_function.rb
+++ app/models/event_function.rb
@@ -6,6 +6,10 @@ class EventFunction < ApplicationRecord
   validates :hourly_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :name, uniqueness: { scope: :event_id, message: "já existe neste evento" }
 
+  def to_s
+    name
+  end
+
   def hourly_rate_formatted
     "R$ #{format('%.2f', hourly_rate)}/h"
   end
