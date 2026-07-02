99fca766e7b4082b1d2c29256e5ebd2a194ff6f3
Daniel Lirio <daniellirioreis@gmail.com>
2026-07-02 00:52:54 -0300

    Run roles and admin seed on build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+bundle exec rails runner db/seeds/01_roles.rb
+bundle exec rails runner db/seeds/02_admin_user.rb
