a2a65f9ef4f9808bd8d96f23496380e5e217f418
Daniel Lirio <daniellirioreis@gmail.com>
2026-07-02 11:22:35 -0300

    Fix seed idempotency: skip admin creation if already exists

--- db/seeds/02_admin_user.rb
+++ db/seeds/02_admin_user.rb
@@ -3,11 +3,16 @@ puts "→ Criando usuário admin..."
 admin_role = Role.find_by!(name: "admin")
 
 admin = User.find_or_initialize_by(email: "admin@backstage.com")
-admin.name  = "Admin"
-admin.phone = "11999999999"
-admin.role  = admin_role
-admin.password = "senha123" if admin.new_record?
-admin.cpf      = "52998224725" if admin.cpf.blank?
-admin.save!
 
-puts "   Admin: admin@backstage.com / senha123"
+if admin.new_record?
+  admin.name     = "Admin"
+  admin.phone    = "11999999999"
+  admin.role     = admin_role
+  admin.password = "senha123"
+  admin.cpf      = "52998224725"
+  admin.save!
+  puts "   Admin criado: admin@backstage.com / senha123"
+else
+  admin.update_columns(role_id: admin_role.id)
+  puts "   Admin já existe, role atualizado."
+end
