af8a222d730466de6234b26513e0ca323d82044c
Daniel Lirio <daniellirioreis@gmail.com>
2026-07-02 12:19:36 -0300

    Sidebar Geral section fully policy-driven: dashboard index + my_schedule

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    return redirect_to my_schedule_user_path(current_user) if current_user.role&.collaborator?
-
     authorize :dashboard, :index?
 
     company_ids = if current_user.admin?
