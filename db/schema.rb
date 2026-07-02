c3026024c08524177d4d9cc04fa4e4a49c2aec1c
Daniel Lirio <daniellirioreis@gmail.com>
2026-07-01 21:06:53 -0300

    fix: disable turbo on invite form so create page renders correctly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_02_000001) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_02_000002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -198,8 +198,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_02_000001) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "cpf", null: false
-    t.string "phone", null: false
+    t.string "cpf"
+    t.string "phone"
     t.bigint "role_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
