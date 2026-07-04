e64525dd3d53f0ace9d76b563ac2a413b4da7e8e
Daniel Lirio <daniellirioreis@gmail.com>
2026-07-04 12:15:37 -0300

    feat: bloqueia criar/editar/excluir setor em eventos encerrados

--- app/policies/sector_policy.rb
+++ app/policies/sector_policy.rb
@@ -3,11 +3,11 @@ class SectorPolicy < ApplicationPolicy
     def resolve = scope.all
   end
 
-  # Estrutural: só no rascunho
-  def create?  = can?("create")  && (user.admin? || event_draft?)
-  def edit?    = can?("update")  && (user.admin? || event_draft?)
-  def update?  = can?("update")  && (user.admin? || event_draft?)
-  def destroy? = can?("destroy") && (user.admin? || event_draft?)
+  # Estrutural: só no rascunho, bloqueado para todos quando encerrado
+  def create?  = can?("create")  && !event_closed? && (user.admin? || event_draft?)
+  def edit?    = can?("update")  && !event_closed? && (user.admin? || event_draft?)
+  def update?  = can?("update")  && !event_closed? && (user.admin? || event_draft?)
+  def destroy? = can?("destroy") && !event_closed? && (user.admin? || event_draft?)
 
   private
 
