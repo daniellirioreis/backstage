a2b9f78adc252ed4a70d7fbad0d01a662e4a57db
Daniel Lirio <daniellirioreis@gmail.com>
2026-07-31 14:24:04 -0300

    feat: adicionar perfis de gestão e coordenação à empresa
    Novos roles em CompanyUser:
    - production_director, executive_producer, producer, event_analyst
    - event_coordinator, operations_coordinator, sector_coordinator
    
    Traduções pt-BR via company_user.roles.*

--- app/models/company_user.rb
+++ app/models/company_user.rb
@@ -2,7 +2,11 @@ class CompanyUser < ApplicationRecord
   belongs_to :company
   belongs_to :user
 
-  ROLES = %w[owner manager operator collaborator].freeze
+  ROLES = %w[
+    owner manager operator collaborator
+    production_director executive_producer producer event_analyst
+    event_coordinator operations_coordinator sector_coordinator
+  ].freeze
 
   validates :role, inclusion: { in: ROLES }
   validates :user_id, uniqueness: { scope: :company_id, message: "já pertence a esta empresa" }
